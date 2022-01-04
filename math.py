42934c3949a99b9aaf2aaf74e1f7be818e86a4c3
chaitra <chaitram1310@gmail.com>
2022-01-04 14:25:11 +0530

    removed fn adder

--- math.py
+++ math.py
@@ -1,19 +1,15 @@
-# add implementation
 def add(x,y):
     return(x+y)
 
-# substract implementation
 def substract(x,y):
     return x-y
 
-# multiply implememtation
 def multiply(x,y):
     return x*y
 
-# division implementation
 def divide(x,y):
     return x/y
-#square implementation
+    
  def square(x):
         pass
 
